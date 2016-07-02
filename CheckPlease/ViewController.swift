d09bdcc651f22f7dea5bdd1136d68bd5a05c8ef9
jakehirzel <hirzel@hotmail.com>
2016-07-02 13:21:43 -0400

    Implemented basic capture of totals and return of tip amounts to view.

--- CheckPlease/ViewController.swift
+++ CheckPlease/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var totalBillField: UITextField!
+    @IBOutlet weak var fifteenTipAmount: UILabel!
+    @IBOutlet weak var eighteenTipAmount: UILabel!
+    @IBOutlet weak var twentyTipAmount: UILabel!
+    
+    let possibleTipPercentages = [0.15, 0.18, 0.20]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set total input as delegate
+        totalBillField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        let totalBillAmount = Double(textField.text!)!
+        for tipPct in possibleTipPercentages {
+            let tipAmount = totalBillAmount * tipPct
+            fifteenTipAmount.text = String(format: "%.2f", tipAmount)
+        }
+    }
 }
 

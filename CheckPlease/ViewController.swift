01a582637c61aeeaa72f6542d4401959c9cdaec7
jakehirzel <hirzel@hotmail.com>
2016-07-06 14:08:51 -0400

    Implemented rest of keys and adjusted formatting of Total Bill label.

--- CheckPlease/ViewController.swift
+++ CheckPlease/ViewController.swift
@@ -20,6 +20,15 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSou
     @IBOutlet weak var keyOne: UIButton!
     @IBOutlet weak var keyTwo: UIButton!
     @IBOutlet weak var keyThree: UIButton!
+    @IBOutlet weak var keyFour: UIButton!
+    @IBOutlet weak var keyFive: UIButton!
+    @IBOutlet weak var keySix: UIButton!
+    @IBOutlet weak var keySeven: UIButton!
+    @IBOutlet weak var keyEight: UIButton!
+    @IBOutlet weak var keyNine: UIButton!
+    @IBOutlet weak var keyZero: UIButton!
+    @IBOutlet weak var keyPoint: UIButton!
+    @IBOutlet weak var keyDelete: UIButton!
     
     // Create instance of TipCalculator class
     let tipCalculatorInstance = TipCalculator()
@@ -69,23 +78,31 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSou
         percentThree.text = String(format: "$%.2f", tipResults.tipThree)
         
     }
+        
+    // MARK: Keyboard Behaviors
+    
+    func keypadButtonTapped(button: UIButton) {
+        if totalBillLabel.text == "$0.00" {
+            totalBillLabel.text = "$"
+            totalBillLabel.text?.appendContentsOf(button.titleLabel!.text!)
+        }
+        else {
+            totalBillLabel.text?.appendContentsOf(button.titleLabel!.text!)
+        }
+    }
     
     // MARK: Actions
     
-    @IBAction func oneKeyTap(sender: UIButton) {
+    @IBAction func numKeyTapped(sender: UIButton) {
         keypadButtonTapped(sender)
     }
     
-    @IBAction func twoKeyTap(sender: UIButton) {
-        keypadButtonTapped(sender)
+    @IBAction func delKeyTapped(sender: UIButton) {
+        if totalBillLabel.text == "$" {
+            return
+        }
+        else {
+            totalBillLabel.text?.removeAtIndex((totalBillLabel.text?.endIndex.predecessor())!)
+        }
     }
-    
-    @IBAction func threeKeyTap(sender: UIButton) {
-        keypadButtonTapped(sender)
-    }
-    
-    func keypadButtonTapped(button: UIButton) {
-        totalBillLabel.text = button.titleLabel!.text
-    }
-    
 }

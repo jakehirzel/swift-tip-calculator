849e9dd5c179d624c76871bff4bab56204791b12
jakehirzel <hirzel@hotmail.com>
2017-11-27 22:07:48 -0500

    Removed deprecated "characters" from KeypadBehavior.

--- CheckPlease/KeypadBehavior.swift
+++ CheckPlease/KeypadBehavior.swift
@@ -13,17 +13,17 @@ class KeypadBehavior {
     func keypadButtonTapped(_ button: UIButton, textIn: String, totalIn: String) -> (textOut: String, totalOut: String) {
         
         // Only allow one .
-        if button.titleLabel!.text == "." && textIn.characters.contains(".") == true {
+        if button.titleLabel!.text == "." && textIn.contains(".") == true {
             return (textOut: textIn, totalOut: totalIn)
         }
         
         // Only allow two characters after a .
-        else if textIn.characters.count > 3 && textIn[(textIn.index(textIn.endIndex, offsetBy: -3))] == "." {
+        else if textIn.count > 3 && textIn[(textIn.index(textIn.endIndex, offsetBy: -3))] == "." {
             return (textOut: textIn, totalOut: totalIn)
         }
         
-        // Reject . after thousands place
-        else if textIn.characters.count == 5 && textIn.characters.contains(".") == false && button.titleLabel?.text != "." {
+        // Limit to thousands place (plus optional decimal and change)
+        else if textIn.count == 5 && textIn.contains(".") == false && button.titleLabel?.text != "." {
             return (textOut: textIn, totalOut: totalIn)
         }
         
